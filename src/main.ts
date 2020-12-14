1e840fd432d65e27b3e11a3a61c85b014a1a7ab7
Giowans <gio_tails@live.com>
2020-12-14 12:13:41 -0600

    Almost ready for Hosting... fixing db conecction issue: Heroku PORT

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  console.log('Corriendo en el puerto: '+process.env.PORT+ ' en modo: '+process.env.NODE_ENV + ' UWU');
-  await app.listen(process.env.PORT || 80);
+  const PORT = process.env.PORT || 8080;
+  console.log('Corriendo en el puerto: '+PORT+ ' en modo: '+process.env.NODE_ENV + ' UWU');
+  await app.listen(PORT);
 }
 bootstrap();
