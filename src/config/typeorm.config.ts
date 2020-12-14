c2e32a7c9a6d5245e62e899cccb4207a332f2cc5
Giowans <gio_tails@live.com>
2020-12-14 15:25:41 -0600

    Almost ready for Hosting... fixing db conecction issue: Heroku PORT

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -11,6 +11,8 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'mongodb',
     url: "mongodb+srv://giowans999:ppip999@ppipcluster.yjwj0.mongodb.net/ppip?retryWrites=true&w=majority",
+    authSource: 'ppip999',
+    replicaSet: "ppipcluster-shard-00-01",
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true,
 }; 
