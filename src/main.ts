39eec9ad84040038f9ccc88cdb58c807702e8026
Giowans <gio_tails@live.com>
2020-12-14 00:52:58 -0600

    Almost ready for Hosting... fixing db conecction issue: PORT issue

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.PORT || 8080);
+  console.log('Corriendo en el puerto: '+process.env.PORT+ ' en modo: '+process.env.NODE_ENV + ' UWU');
 }
 bootstrap();
