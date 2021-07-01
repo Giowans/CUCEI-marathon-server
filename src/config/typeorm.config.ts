e4fda8e0e38f7e91d126a286bc4ae89efb32c0e0
Giowans <gio_tails@live.com>
2021-06-30 21:50:46 -0500

    Conserve dev typeorm config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -8,13 +8,3 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
     synchronize: true,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
 };
-
-/*export const typeOrmConfig: TypeOrmModuleOptions = {
-    type: 'mongodb',
-    url: "mongodb+srv://giowans999:ppip999@ppipcluster-shard-00-01.yjwj0.mongodb.net:27017/ppip?retryWrites=true&w=majority",
-    ssl: true,
-    authSource: 'ppip999',
-    replicaSet: "ppipcluster-shard-00-01",
-    entities: [__dirname + '/../**//*.entity.{js,ts}'],
-    synchronize: true,
-};*/
